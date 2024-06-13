e97f939e30d1afc1be2d4edaf9fc0e9f80c921b1
mayur.c <mayur.c@position2.com>
2024-06-13 12:54:25 +0530

    type script learn init

---
+++ src/2. primitives.ts
@@ -0,0 +1,9 @@
+let isPresent: boolean = true;
+let magic: number = 1;
+let hello: string = "hello";
+
+let notDefined: undefined = undefined;
+let notPresent: null = null;
+
+let penta: symbol = Symbol("star");
+let biggy: bigint = 12n;
